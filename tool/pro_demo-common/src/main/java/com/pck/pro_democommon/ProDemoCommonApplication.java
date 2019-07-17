ae8187f4a14230d9843e8d83b4a9514d330a961c
15258334873 <1030319889@qq.com>
2019-07-17 16:56:47 +0800

    工具模块的提交

---
+++ tool/pro_demo-common/src/main/java/com/pck/pro_democommon/ProDemoCommonApplication.java
@@ -0,0 +1,13 @@
+package com.pck.pro_democommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProDemoCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProDemoCommonApplication.class, args);
+    }
+
+}
