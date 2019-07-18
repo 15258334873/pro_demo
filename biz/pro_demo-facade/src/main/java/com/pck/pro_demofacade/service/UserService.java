76929b468ec160a1ce23e40663feefd3a31c7c55
15258334873 <1030319889@qq.com>
2019-07-18 14:05:13 +0800

    项目架构完整第三次提交

---
+++ biz/pro_demo-facade/src/main/java/com/pck/pro_demofacade/service/UserService.java
@@ -0,0 +1,11 @@
+package com.pck.pro_demofacade.service;
+
+/**
+ * @description: 用户业务
+ * @program: pro_demo
+ * @author: pancikai
+ * @create: 2019/07/18 14:03
+ * @version: 1.0
+ */
+public interface UserService {
+}
